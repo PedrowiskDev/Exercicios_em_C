f8682005131ae427275242d3101baad957fc0ea1
Pedro Henrique Caetano Soares <1373928@sga.pucminas.br>
2023-10-18 18:26:32 -0300

    Exercicio 7 pronto.

---
+++ Lista 6/Exercicio7.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char str[10];
+    int contadorVogais, contadorConsoantes;
+
+    printf("Digite um texto: ");
+    scanf("%s", str);
+
+    for (int i = 0; i < 10; i++)
+    {
+
+        if (str[i] >= 'A' || str[i] <= 'Z' && str[i] >= 'a' || str[i] <= 'z')
+        {
+            contadorConsoantes++;
+            if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+            {
+                contadorVogais++;
+                contadorConsoantes--;
+            }
+        }
+    }
+
+    printf("Numero de vogais: %d\n", contadorVogais);
+    printf("Numero de consoantes: %d\n", contadorConsoantes);
+}
