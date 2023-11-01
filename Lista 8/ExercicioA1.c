224244c8b77909b22cfb00e2c3412c41c21b1f09
Pedro Henrique Caetano Soares <1373928@sga.pucminas.br>
2023-11-01 18:22:21 -0300

    Lista 8

---
+++ Lista 8/ExercicioA1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *arquivo = fopen("arquivo-01.txt", "r+");
+    char palavras[100];
+    int contador = 0;
+
+    if (arquivo == NULL)
+    {
+        printf("Erro ao abrir arquivo!");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        printf("Arquivo encontrado! \n");
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (fgets(palavras, 100, arquivo) != NULL)
+            {
+                contador++;
+            }
+        }
+
+        fclose (arquivo);
+    }
+
+    printf("A quantidade de linhas é:", contador);
+
+    return 0;
+}
